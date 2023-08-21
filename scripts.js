25d4363e48f164d3d8655015d64491e97fb604e3
amjwaburg <amjwarburg@gmail.com>
2023-08-21 20:48:51 +0100

    Add simple operations to JavaScript and calc UI
    This commit simply adds a UI for the calculator (made with CSS and
    HTML) as well as the simplest of calculating functions in the
    JavaScript file.
    
    The next stage will be to connect the UI to some functionality and
    the afformentioned functions.

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let numA
+let operator
+let numB
+
+function operate(numA, operator, numB) {
+    
+}
